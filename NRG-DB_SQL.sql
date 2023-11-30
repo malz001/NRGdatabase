288dc5394348e5e9f5daf3b59618e70ccb545b1d
malz001 <75031731+malz001@users.noreply.github.com>
2023-11-30 19:25:44 +0000

    Update NRG-DB_SQL.sql

--- NRG-DB_SQL.sql
+++ NRG-DB_SQL.sql
@@ -3,108 +3,108 @@ USE MyDB;
 CREATE TABLE Employee_Contact_Info
 (
 	id INT AUTO_INCREMENT NOT NULL,
-	Employee-Number VARCHAR(10),
+	Employee_Number VARCHAR(10),
     Surname VARCHAR(30),
-	First-Name VARCHAR(30),
+	First_Name VARCHAR(30),
 	DOB VARCHAR(15),
-    Phone-Number VARCHAR(15),
+    Phone_Number VARCHAR(15),
 	Email VARCHAR(30),
     PRIMARY KEY (id)
 );
-CREATE TABLE Phone-Number
+CREATE TABLE Work_Info
 (
 	id INT AUTO_INCREMENT NOT NULL,
-    Employee-Number VARCHAR(10),
-	First-Day VARCHAR(30),
+    Employee_Number VARCHAR(10),
+	First_Day VARCHAR(30),
     Office VARCHAR(30),
 	Skills VARCHAR(30),
-	Current-Work VARCHAR(30),
-	Booked-Leave VARCHAR(30),
+	Current_Work VARCHAR(30),
+	Booked_Leave VARCHAR(30),
 	Leave VARCHAR(30),
-	Personal-Leave VARCHAR(30),
+	Personal_Leave VARCHAR(30),
     PRIMARY KEY (id)
 );
 
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('001', 'Smith', 'Jake', '01/01/1997', '07058555346', 'Smith_Jake@outlook.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('002', 'Brown', 'Liam', '34510', '07784047808', 'Brown_Liam@outlook.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email);
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email);
 VALUES
 ('003', 'Jones', 'Kate', '35587', '07749966923', 'Jones_Kate@outlook.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('004', 'Smalls', 'Jake', '32855', '07064107648', 'Smalls_Jake@outlook.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('005', 'Coxs', 'Courtney', '05/25/1985', '07956785296', 'Coxs_Courtney@outlook.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('006', 'Geller', 'Rachael', '18519', '07866573071', 'Geller_Rachael@outlook.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('007', 'Lin', 'Linda', '36179', '07710572387', 'Lin_Linda@outlook.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('008', 'Rin', 'Kai', '35745', '07870938832', 'Rin_Kai@outlook.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('009', 'Long', 'Chris', '20090', '07910398376', 'Long_Chris@yahoo.com');
-INSERT INTO Employee-Contact-Info
-(Employee-Number,Surname, First-Name, DOB, Phone-Number, Email)
+INSERT INTO Employee_Contact_Info
+(Employee_Number,Surname, First_Name, DOB, Phone_Number, Email)
 VALUES
 ('010', 'Carlton', 'Greg', '27518', '07719258608', 'Carlton_Greg@outlook.com');
-SELECT * FROM Employee-Contact-Info;
+SELECT * FROM Employee_Contact_Info;
 
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('001', '02/05/2021', 'London', 'IT', 'Project-X', '01/03/2024 - 31/03/2024', '160/h', '56/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('002', '08/07/2020', 'London', 'Engineer', 'Project-X', 'Nil', '95/h', '55/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('003', '26/09/2018', 'Cheltenham', 'Engineer', 'Project-J', 'Nil', '20/h', '80/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('004', '19/01/2020', 'Isleworth', 'Engineer', 'Project-X', 'Nil', '80/h', '50/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('005', '02/05/2021', 'London', 'Hr ', 'On Going', 'Nil', '10/h', '80/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('006', '19/08/2021', 'Slough', 'IT', 'Project-J', '02/09/2024 - 20/09/2024', '160/h', '45/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('007', '22/05/2010', 'Slough', 'Logistics', 'On Going', 'Nil', '100/h', '70/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('008', '02/05/2021', 'London', 'Adminstration', 'Project-X', 'Nil', '140/h', '10/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('009', '12/01/1998', 'Cheltenham', 'Financial Advisor', 'On Going', '18/12/2023 - 05/01/2024', '160/h', '60/h');
-INSERT INTO Work-Info
-(Employee-Number,First-Day, Office, Skills, Current-Work, Booked-Leave, Leave,Personal-Leave)
+INSERT INTO Work_Info
+(Employee_Number,First_Day, Office, Skills, Current_Work, Booked_Leave, Leave,Personal_Leave)
 VALUES
 ('010', '02/05/2021', 'London', 'Logistics', 'Project-X', 'Nil', '90/h', '80/h');
-SELECT * FROM Work-Info;
+SELECT * FROM Work_Info;
